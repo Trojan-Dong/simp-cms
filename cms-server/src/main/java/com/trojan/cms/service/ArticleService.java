dccd9b04356f3973e9ee6a4910ba2249ef7e6edd
董国军 <dongguojun@cbib.com>
2024-01-26 14:52:21 +0800

    [feat]project init

---
+++ cms-server/src/main/java/com/trojan/cms/service/ArticleService.java
@@ -0,0 +1,16 @@
+package com.trojan.cms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.trojan.cms.entity.Article;
+
+/**
+ * <p>
+ * 文章 服务类
+ * </p>
+ *
+ * @author author
+ * @since 2021-04-12
+ */
+public interface ArticleService extends IService<Article> {
+
+}
